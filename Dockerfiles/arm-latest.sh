ac0a9a19ef926ed61c19a60b6397ede840b84ce5
Nick Warner <nick@Nicks-MBP.whi.local>
2020-04-30 22:49:01 -0600

    Push the latest ARM image after building it.

--- Dockerfiles/arm-latest.sh
+++ Dockerfiles/arm-latest.sh
@@ -1,1 +1,2 @@
 docker build -t portableprogrammer/status-light:arm-0.1 -t portableprogrammer/status-light:arm-latest -f ~/projects/Status-Light/Dockerfiles/Dockerfile .
+docker push portableprogrammer/status-light:arm-latest
