3afaf23f8b1e88d82929b1dbd3dab8e0cec5f155
Nick Warner <nick@Nicks-MBP.whi.local>
2020-04-30 22:28:19 -0600

    Loop the main logic for use in a Docker image.

--- status-light/status-light.py
+++ status-light/status-light.py
@@ -3,20 +3,27 @@
 
 import webex
 import os
+import time
 
-# Webex Status
-personID = os.environ['WEBEX_PERSONID']
-webexAPI = webex.WebexAPI()
-webexAPI.botKey = os.environ['WEBEX_BOTID']
-webexStatus = webexAPI.getPersonStatus(personID)
+try:
+    while True:
+        # Webex Status
+        personID = os.environ['WEBEX_PERSONID']
+        webexAPI = webex.WebexAPI()
+        webexAPI.botKey = os.environ['WEBEX_BOTID']
+        webexStatus = webexAPI.getPersonStatus(personID)
 
-# O365 Status (based on calendar)
+        # O365 Status (based on calendar)
 
-# Take the statii and determine what we care about most
-# White/Green/Off when available
-# Blue when in a meeting (Calendar)
-# Red when on a call or DnD (due to the way that Teams handles status priorities) - This takes priority over calendar
+        # Take the statii and determine what we care about most
+        # White/Green/Off when available
+        # Blue when in a meeting (Calendar)
+        # Red when on a call or DnD (due to the way that Teams handles status priorities) - This takes priority over calendar
 
-# Set the Calendar first, then let Webex override it
+        # Set the Calendar first, then let Webex override it
 
-print(webexStatus)
+        print(webexStatus)
+        time.sleep(15)
+
+except:
+    print('Exception!')
