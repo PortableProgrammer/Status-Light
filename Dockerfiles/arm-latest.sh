7b33496e1236cfce5963899408f54bddc32c0bc0
PortableProgrammer <nick@warnerheavyindustries.com>
2020-05-27 15:27:17 -0600

    Bump ARM version to 1.0

--- Dockerfiles/arm-latest.sh
+++ Dockerfiles/arm-latest.sh
@@ -1,2 +1,2 @@
-docker build -t portableprogrammer/status-light:arm-0.1 -t portableprogrammer/status-light:arm-latest -f ~/projects/Status-Light/Dockerfiles/Dockerfile .
+docker build -t portableprogrammer/status-light:arm-1.0 -t portableprogrammer/status-light:arm-latest -f ~/projects/Status-Light/Dockerfiles/Dockerfile .
 docker push portableprogrammer/status-light:arm-latest
